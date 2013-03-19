979ef782ae3138a95819678e69b42ff96c4211f3
Carlos Vilhena <carlosvilhena@gmail.com>
2013-03-19 14:32:50 +0000

    no escape mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,18 @@
-set -e
-
 echo ""
-echo -e "\033[32m(  __  \ (  ___  )\__   __/(  ____ \\__   __/( \      (  ____ \(  ____ \ \033[0m"
-echo -e "\033[32m| (  \  )| (   ) |   ) (   | (    \/   ) (   | (      | (    \/| (    \/ \033[0m"
-echo -e "\033[32m| |   ) || |   | |   | |   | (__       | |   | |      | (__    | (_____  \033[0m"
-echo -e "\033[32m| |   | || |   | |   | |   |  __)      | |   | |      |  __)   (_____  ) \033[0m"
-echo -e "\033[32m| |   ) || |   | |   | |   | (         | |   | |      | (            ) | \033[0m"
-echo -e "\033[32m| (__/  )| (___) |   | |   | )      ___) (___| (____/\| (____/\/\____) | \033[0m"
-echo -e "\033[32m(______/ (_______)   )_(   |/       \_______/(_______/(_______/\_______) \033[0m"
+echo "\033[32m(  __  \ (  ___  )\__   __/(  ____ \\__   __/( \      (  ____ \(  ____ \ \033[0m"
+echo "\033[32m| (  \  )| (   ) |   ) (   | (    \/   ) (   | (      | (    \/| (    \/ \033[0m"
+echo "\033[32m| |   ) || |   | |   | |   | (__       | |   | |      | (__    | (_____  \033[0m"
+echo "\033[32m| |   | || |   | |   | |   |  __)      | |   | |      |  __)   (_____  ) \033[0m"
+echo "\033[32m| |   ) || |   | |   | |   | (         | |   | |      | (            ) | \033[0m"
+echo "\033[32m| (__/  )| (___) |   | |   | )      ___) (___| (____/\| (____/\/\____) | \033[0m"
+echo "\033[32m(______/ (_______)   )_(   |/       \_______/(_______/(_______/\_______) \033[0m"
 echo ""
 
 BASE_DIR=~
 DOT_FILES_DIR=$BASE_DIR/dotfiles
 
 function symlink_dotfiles {
-  echo -e "\033[32m  => Creating symlinks... \033[0m"
+  echo "\033[32m  => Creating symlinks... \033[0m"
   cd $DOT_FILES_DIR
   for file in symlinks/*
   do
@@ -30,9 +28,9 @@ function symlink_dotfiles {
 }
 
 function clone_dotfiles_repo {
-  echo -e "\033[32m  => Cloning dotfiles repo to $BASE_DIR \033[0m"
+  echo "\033[32m  => Cloning dotfiles repo to $BASE_DIR \033[0m"
   if [ -d "$BASE_DIR/dotfiles" ]; then
-      echo -e '\033[32m    + Repo exists, fetching latest changes \033[0m'
+      echo '\033[32m    + Repo exists, fetching latest changes \033[0m'
       cd $BASE_DIR/dotfiles
       git reset --hard
       git checkout master
@@ -44,21 +42,21 @@ function clone_dotfiles_repo {
 }
 
 function setup_vim {
-  echo -e "\033[32m  => Setup .vim \033[0m"
-  echo -e '\033[32m    + Creating directories (bundle,tmp,backup,autoload) \033[0m'
+  echo "\033[32m  => Setup .vim \033[0m"
+  echo '\033[32m    + Creating directories (bundle,tmp,backup,autoload) \033[0m'
   mkdir -p $BASE_DIR/.vim/{bundle,tmp,backup,autoload}
-  echo -e '\033[32m    + Cloning vundle \033[0m'
+  echo '\033[32m    + Cloning vundle \033[0m'
   git clone https://github.com/gmarik/vundle.git $BASE_DIR/.vim/bundle/vundle
-  echo -e '\033[32m    + Installign bundle \033[0m'
+  echo '\033[32m    + Installign bundle \033[0m'
   vim +BundleInstall +qall
 }
 
 function finish {
-  echo -e "\033[32m  => Finishing up \033[0m"
-  echo -e '\033[32m    + Copying bash completion into place \033[0m'
+  echo "\033[32m  => Finishing up \033[0m"
+  echo '\033[32m    + Copying bash completion into place \033[0m'
   cp $DOT_FILES_DIR/git-completion.bash $BASE_DIR
-  echo -e '\033[32m    + Now, install the fonts and the zsh theme manually! \033[0m'
-  echo -e "\033[32m  => Done! \033[0m"
+  echo '\033[32m    + Now, install the fonts and the zsh theme manually! \033[0m'
+  echo "\033[32m  => Done! \033[0m"
 }
 
 function setup {
